fbd9db6a4c6ef234ee90f813e75ed8b013ede33c
Shawn <coffee.or.tea88@gmail.com>
2019-02-28 13:43:45 +0800

    Add Prisma. 'Resolvers' folder structure

--- src/index.js
+++ src/index.js
@@ -5,25 +5,32 @@ import {
   ApolloServer,
   gql
 } from "apollo-server-express";
+import { prisma } from './generated/prisma-client'
+const Link = require('./resolvers/Link')
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+const fs = require('fs');
+const typeDefs = gql(fs.readFileSync("./src/schema.graphql", "utf8").toString())
+
 
 const resolvers = {
   Query: {
     hello: () => "world"
-  }
-};
+  },
+  Link
+}
 
 const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  path: "/graphql"
+  path: "/graphql",
+  context: request => {
+    return {
+      ...request,
+      prisma
+    }
+  }
 });
 
 server.applyMiddleware({
